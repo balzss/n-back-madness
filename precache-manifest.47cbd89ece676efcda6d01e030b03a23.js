0b5bbae3100c40be81ca43a2204acacfd4236b04
balazs.saros <balazs.saros@gmail.com>
2019-10-03 23:21:11 +0200

    Updates

---
+++ precache-manifest.47cbd89ece676efcda6d01e030b03a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9662e4eb164f26b496633407d20e4e6e",
+    "url": "/n-back-madnass/index.html"
+  },
+  {
+    "revision": "92ff934e69b68c55da0f",
+    "url": "/n-back-madnass/static/css/main.f612ed7a.chunk.css"
+  },
+  {
+    "revision": "ab35a377e5368a52ac60",
+    "url": "/n-back-madnass/static/js/2.e552c2ad.chunk.js"
+  },
+  {
+    "revision": "92ff934e69b68c55da0f",
+    "url": "/n-back-madnass/static/js/main.730b5622.chunk.js"
+  },
+  {
+    "revision": "2fdd1c8fc45a7310d5b4",
+    "url": "/n-back-madnass/static/js/runtime-main.4dc5d5fb.js"
+  }
+]);
